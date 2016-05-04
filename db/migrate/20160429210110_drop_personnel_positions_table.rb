9b551bdd874479d94e9a9a46cb27d520f09d37e9
Cameron Reiter <cameron@intrinsicbusinessconsulting.com>
2016-05-04 19:25:09 +0000

    Clean up migration files

--- db/migrate/20160429210110_drop_personnel_positions_table.rb
+++
@@ -1,5 +0,0 @@
-class DropPersonnelPositionsTable < ActiveRecord::Migration
-  def change
-    drop_table :personnel_positions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429210110) do
+ActiveRecord::Schema.define(version: 20160503223158) do
 
   create_table "apparatu_positions", force: :cascade do |t|
     t.integer  "apparatu_id"
