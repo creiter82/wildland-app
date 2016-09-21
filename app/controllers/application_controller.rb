e54f9edd0e0ed117483c741f539e798526e50333
Cameron Reiter <cameron@intrinsicbusinessconsulting.com>
2016-09-21 18:29:02 +0000

    Edit redirect when logging in to rosters index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_personnel!
   
+  def after_sign_in_path_for(resource)
+    rosters_path
+  end
+  
   private
   
     def is_admin?
