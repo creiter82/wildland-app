75c9122bcef390f891aa559d2ef3416bbf3a3a90
Cameron Reiter <cameron@intrinsicbusinessconsulting.com>
2017-06-26 01:13:50 +0000

    Implement helper method to allow for deployment emails

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -1,2 +1,11 @@
 module RostersHelper
+  
+  def deployment_email_helper(roster)
+    @deploy = @roster.assignments.where(alternate: false)
+    @can_deploy = @deploy.where(deploy: false)
+    if !@can_deploy.any?
+      link_to 'Send Deployment Email', '#', class: 'btn btn-success'
+    end
+  end
+  
 end
