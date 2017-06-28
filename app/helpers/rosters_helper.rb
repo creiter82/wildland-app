7988c5270754632fdb74a22185bc764f01b3908d
Cameron Reiter <cameron@intrinsicbusinessconsulting.com>
2017-06-28 19:05:28 +0000

    Update deployment mailer to be apparatus specific

--- app/helpers/rosters_helper.rb
+++ app/helpers/rosters_helper.rb
@@ -1,7 +1,8 @@
 module RostersHelper
   
-  def deployment_email_helper(roster)
-    @deploy = @roster.assignments.where(alternate: false)
+  def deployment_email_helper(roster, app)
+    @apparatus = app
+    @deploy = @roster.assignments.where(alternate: false, apparatu_id: @apparatus)
     @can_deploy = @deploy.where(deploy: false)
     if !@can_deploy.any? && @roster.assignments.any?
       link_to 'Send Deployment Email', send_mail_path, id: @roster.id, class: 'btn btn-success'
